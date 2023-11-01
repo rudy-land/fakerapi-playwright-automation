70c099515997bc6a536e4c86ba0cbb1c14034792
rudy-land <148643914+rudy-land@users.noreply.github.com>
2023-11-01 16:17:08 -0700

    Update addresses.test.ts
    Expanded and added test case

--- tests/addresses.test.ts
+++ tests/addresses.test.ts
@@ -2,6 +2,26 @@ import { test, expect } from '@playwright/test';
 
 test('should retrieve one Address', async ({ request }) => {
     const anAddress = await request.get(`/api/v1/addresses?_quantity=1`);
-    console.log(anAddress.url());
+
+    // Response should be successful
     expect(anAddress.ok()).toBeTruthy();
+
+    // Should return 200 and one record
+    expect(await anAddress.json()).toEqual(expect.objectContaining({
+        "code": 200,
+        "total": 1
+      }));
+});
+
+test('should retrieve multiple Addresses', async ({ request }) => {
+    const anAddress = await request.get(`/api/v1/addresses?_quantity=5`);
+
+    // Response should be successful
+    expect(anAddress.ok()).toBeTruthy();
+
+    // Should return 200 and 5 records
+    expect(await anAddress.json()).toEqual(expect.objectContaining({
+        "code": 200,
+        "total": 5
+      }));
 });
