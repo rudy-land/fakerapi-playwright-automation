544f34971e9048bf33e99b1fe8755a9667a83729
rudy-land <148643914+rudy-land@users.noreply.github.com>
2023-11-01 15:29:59 -0700

    Update addresses.test.ts
    Fixed URL

--- tests/addresses.test.ts
+++ tests/addresses.test.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('should retrieve one Address', async ({ request }) => {
-    const anAddress = await request.get(`/repos/api/v1/addresses?_quantity=1`);
+    const anAddress = await request.get(`/api/v1/addresses?_quantity=1`);
+    console.log(anAddress.url());
     expect(anAddress.ok()).toBeTruthy();
 });
