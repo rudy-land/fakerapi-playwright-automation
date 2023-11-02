75b6db45a33ca820918c790b56cf465210a23e4e
rudy-land <148643914+rudy-land@users.noreply.github.com>
2023-11-01 23:49:47 -0700

    Update personResource.ts

--- resources/personResource.ts
+++ resources/personResource.ts
@@ -1,1 +1,9 @@
 
+// Here define a class PersonResource
+
+// This class has a:
+// // const to hold '/api/v1/persons'
+
+// // a method to call a response with a the following optional parameters
+// // something like personResource.requestGet(quantity, gender, bdayStart, bdayEnd)
+// // it returns something like `/api/v1/persons?_quantity=1&_gender=female`
