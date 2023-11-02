f7dcec0809496a53508621d27bad018bad7827a4
rudy-land <148643914+rudy-land@users.noreply.github.com>
2023-11-01 23:49:24 -0700

    Update addressResource.ts

--- resources/addressResource.ts
+++ resources/addressResource.ts
@@ -1,1 +1,9 @@
+// Here define a class AddressResource
+
+// This class has a:
+// // const to hold '/api/v1/addresses'
+
+// // a method to call a response with a quantity parameter
+// // something like addressResource.requestGet(quantity)
+// // it returns something like `/api/v1/addresses?_quantity=1`
 
